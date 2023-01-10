e2beef59e8174b99e9685ff774fbfa5c5c8db716
brightchul <abhbtbb1@gmail.com>
2023-01-10 20:47:56 +0900

    feat: 3.1.8 하위 도메인 라우팅 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,13 @@ import { AppService } from './app.service';
 import { UsersController } from './users/users.controller';
 import { UsersModule } from './users/users.module';
 import { UsersService } from './users/users.service';
+import { ApiController } from './api/api.controller';
 
 @Module({
   imports: [UsersModule],
-  controllers: [AppController, UsersController],
+
+  // 하위 도메인 라우팅시 먼저 와야 한다.
+  controllers: [ApiController, AppController, UsersController],
   providers: [AppService, UsersService],
 })
 export class AppModule {}
